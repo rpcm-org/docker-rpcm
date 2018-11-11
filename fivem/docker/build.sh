bfe8d814c84225ee14ab8c25869e70b6dbe24c92
celestian <petr.celestian@gmail.com>
2018-11-11 15:59:55 +0100

    Add fivem container with MariaDB
    label: feature
    
    Resolves #1 #2

---
+++ fivem/docker/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir -p /app/fivem/server
+mkdir -p /app/fivem/server-data
+
+# FiveM server
+wget http://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/${RPCM_FIVEM_VERSION}/fx.tar.xz
+tar xf ./fx.tar.xz \
+    --exclude alpine/dev \
+    --exclude alpine/proc \
+    --exclude alpine/run \
+    --exclude alpine/sys \
+    -C ./server
+rm -f ./fx.tar.xz
---
+++ fivem/docker/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+sleep 10
+
+export RPCM_LOCAL_IP=`getent hosts fivem | awk '{ print $1 }'`
+export RPCM_DB_IP=`getent hosts rpcm_db | awk '{ print $1 }'`
+
+# Configuration
+if [ ! -f config_initialized.mark ]; then
+    cp conf/server.cfg.in server-data/server.cfg
+    sed -i -e "s/{RPCM_HOSTNAME}/'${RPCM_HOSTNAME}'/g" server-data/server.cfg
+    sed -i -e "s/{RPCM_MAXCLIENTS}/'${RPCM_MAXCLIENTS}'/g" server-data/server.cfg
+    sed -i -e "s/{RPCM_LICENSEKEY}/'${RPCM_LICENSEKEY}'/g" server-data/server.cfg
+    touch mark.config
+fi
+
+if [ ! -f mysql_initialized.mark ]; then
+    sed -i -e "s/{RPCM_LOCAL_IP}/'${RPCM_LOCAL_IP}'/g" db_data/init.sql
+    sed -i -e "s/{RPCM_DB_NAME}/'${RPCM_DB_NAME}'/g" db_data/init.sql
+    sed -i -e "s/{RPCM_DB_USER}/'${RPCM_DB_USER}'/g" db_data/init.sql
+    sed -i -e "s/{RPCM_DB_PASS}/'${RPCM_DB_PASS}'/g" db_data/init.sql
+
+    mysql -h rpcm_db -u root --password=${RPCM_DB_ROOT_PASSWORD} < db_data/init.sql >>import.log 2>&1
+
+    touch mark.rpcm_db
+    sleep 2
+fi
+
+# Start and stop FiveM
+cd server-data
+bash /app/fivem/server/run.sh +exec server.cfg >> ../fivem.log  2>&1 &
+echo $! > ../fivem_0.pid
+sleep 10
+kill -9 `cat ../fivem_0.pid`
+rm ../fivem_0.pid
+sleep 5
+
+# Final start of FiveM
+touch mark.fivem
+bash /app/fivem/server/run.sh +exec server.cfg >> ../fivem.log 2>&1
