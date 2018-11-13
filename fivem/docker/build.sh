a0b92baab0ee479a6139be430070972e50bad704
celestian <petr.celestian@gmail.com>
2018-11-13 16:53:26 +0100

    Refactor on build.sh
    label: technical debt
    
    This patch move the content of build.sh to Dockerfile,
    so image will have more stages.

--- fivem/docker/build.sh
+++ fivem/docker/build.sh
@@ -2,21 +2,3 @@
 
 mkdir -p /app/fivem/server
 mkdir -p /app/fivem/server-data
-
-# FiveM server
-wget http://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/${RPCM_FIVEM_VERSION}/fx.tar.xz
-tar xf ./fx.tar.xz \
-    --exclude alpine/dev \
-    --exclude alpine/proc \
-    --exclude alpine/run \
-    --exclude alpine/sys \
-    -C ./server
-rm -f ./fx.tar.xz
-
-# CFX-SERVER-DATA
-# branch: master | hash: 1624342
-git clone https://github.com/citizenfx/cfx-server-data server-data
-
-# FIVEM-MYSQL-ASYNC
-# branch: master | hash: 296a2c6
-git clone https://github.com/brouznouf/fivem-mysql-async server-data/resources/mysql-async
