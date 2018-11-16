52b589e9af69ad785c1700c153f548be44554a51
celestian <petr.celestian@gmail.com>
2018-11-16 02:58:06 +0100

    Creation of RPCM database
    label: feature
    
    Division of competencies of individual dockers images:
    * fivem -- all configuration needed for run Fivem, but without creation
               of RPCM database
    * rpcm -- the same as fivem, but with running all necessary scripts
               to create RPCM databse
    
    Resolves #14

--- fivem/docker/entrypoint.sh
+++ fivem/docker/entrypoint.sh
@@ -2,9 +2,6 @@
 
 sleep 10
 
-export RPCM_LOCAL_IP=`getent hosts fivem | awk '{ print $1 }'`
-export RPCM_DB_IP=`getent hosts rpcm_db | awk '{ print $1 }'`
-
 # Configuration
 if [ ! -f config_initialized.mark ]; then
     cp conf/server.cfg.in server-data/server.cfg
@@ -17,18 +14,6 @@ if [ ! -f config_initialized.mark ]; then
     sed -i -e "s/{RPCM_DB_PASS}/${RPCM_DB_PASS}/g" server-data/server.cfg
 
     touch mark.config
-fi
-
-if [ ! -f mysql_initialized.mark ]; then
-    sed -i -e "s/{RPCM_LOCAL_IP}/${RPCM_LOCAL_IP}/g" db_data/init.sql
-    sed -i -e "s/{RPCM_DB_NAME}/${RPCM_DB_NAME}/g" db_data/init.sql
-    sed -i -e "s/{RPCM_DB_USER}/${RPCM_DB_USER}/g" db_data/init.sql
-    sed -i -e "s/{RPCM_DB_PASS}/${RPCM_DB_PASS}/g" db_data/init.sql
-
-    mysql -h rpcm_db -u root --password=${RPCM_DB_ROOT_PASSWORD} < db_data/init.sql >>import.log 2>&1
-
-    touch mark.rpcm_db
-    sleep 2
 fi
 
 # Start and stop FiveM
---
+++ rpcm/docker/entrypoint.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+sleep 10
+
+export RPCM_LOCAL_IP=`getent hosts fivem | awk '{ print $1 }'`
+export RPCM_DB_IP=`getent hosts rpcm_db | awk '{ print $1 }'`
+
+# Configuration
+if [ ! -f config_initialized.mark ]; then
+    cp conf/server.cfg.in server-data/server.cfg
+    sed -i -e "s/{RPCM_HOSTNAME}/${RPCM_HOSTNAME}/g" server-data/server.cfg
+    sed -i -e "s/{RPCM_MAXCLIENTS}/${RPCM_MAXCLIENTS}/g" server-data/server.cfg
+    sed -i -e "s/{RPCM_LICENSEKEY}/${RPCM_LICENSEKEY}/g" server-data/server.cfg
+
+    sed -i -e "s/{RPCM_DB_NAME}/${RPCM_DB_NAME}/g" server-data/server.cfg
+    sed -i -e "s/{RPCM_DB_USER}/${RPCM_DB_USER}/g" server-data/server.cfg
+    sed -i -e "s/{RPCM_DB_PASS}/${RPCM_DB_PASS}/g" server-data/server.cfg
+
+    touch mark.config
+fi
+
+# Database initialization
+if [ ! -f mysql_initialized.mark ]; then
+
+    cp server-data/resources/rpcm/sql/create_database.sql.in server-data/resources/rpcm/sql/create_database.sql
+    cp server-data/resources/rpcm/sql/grant_user_to_database.sql.in server-data/resources/rpcm/sql/grant_user_to_database.sql
+
+    sed -i -e "s/{RPCM_DB_NAME}/${RPCM_DB_NAME}/g" server-data/resources/rpcm/sql/create_database.sql
+    sed -i -e "s/{RPCM_DB_CHARSET}/${RPCM_DB_CHARSET}/g" server-data/resources/rpcm/sql/create_database.sql
+    sed -i -e "s/{RPCM_DB_COLLATE}/${RPCM_DB_COLLATE}/g" server-data/resources/rpcm/sql/create_database.sql
+
+    sed -i -e "s/{RPCM_LOCAL_IP}/${RPCM_LOCAL_IP}/g" server-data/resources/rpcm/sql/grant_user_to_database.sql
+    sed -i -e "s/{RPCM_DB_NAME}/${RPCM_DB_NAME}/g" server-data/resources/rpcm/sql/grant_user_to_database.sql
+    sed -i -e "s/{RPCM_DB_USER}/${RPCM_DB_USER}/g" server-data/resources/rpcm/sql/grant_user_to_database.sql
+    sed -i -e "s/{RPCM_DB_PASS}/${RPCM_DB_PASS}/g" server-data/resources/rpcm/sql/grant_user_to_database.sql
+
+    mysql -h rpcm_db -u root --password=${RPCM_DB_ROOT_PASSWORD} < server-data/resources/rpcm/sql/create_database.sql >>import.log 2>&1
+    mysql -h rpcm_db -u root --password=${RPCM_DB_ROOT_PASSWORD} < server-data/resources/rpcm/sql/grant_user_to_database.sql >>import.log 2>&1
+    mysql -h rpcm_db -u root --password=${RPCM_DB_ROOT_PASSWORD} ${RPCM_DB_NAME} < server-data/resources/rpcm/sql/schema.sql >>import.log 2>&1
+
+    touch mark.rpcm_db
+    sleep 2
+fi
+
+# Start and stop FiveM
+cd server-data
+bash /app/fivem/server/run.sh +exec server.cfg >> ../fivem.log  2>&1 &
+echo $! > ../fivem_0.pid
+sleep 10
+kill -9 `cat ../fivem_0.pid`
+rm ../fivem_0.pid
+sleep 5
+
+# Final start of FiveM
+touch mark.fivem
+bash /app/fivem/server/run.sh +exec server.cfg >> ../fivem.log 2>&1
