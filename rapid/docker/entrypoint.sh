34632a8122f2f82cd9689a4f8b401700c2a4e09d
celestian <petr.celestian@gmail.com>
2018-12-02 17:55:41 +0100

    Add rapid docker

---
+++ rapid/docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source .tox/py37/bin/activate
+python utils.py db_init ${RPCM_DB_FILE}
+uwsgi \
+    --http 0.0.0.0:80 \
+    --wsgi-file main.py \
+    --callable app \
+    --master \
+    --processes 5 \
+    --logto /app/rapid/var/rapid.log \
+    --pidfile /tmp/rapid.pid
